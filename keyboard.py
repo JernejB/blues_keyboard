d5b07d9fe0667872d23c809de5b6a3575ed65f18
JernejB <jernej@coderdojo.si>
2014-10-17 13:38:08 +0200

    Trying different stuff.

--- keyboard.py
+++ keyboard.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import pygame, fluidsynth
 from pygame.locals import *
 
@@ -7,15 +10,14 @@ import fluidsynth
 def generate_scale(tone):
     return [tone,tone+3,tone+5,tone+6,tone+7,tone+10,tone+11,tone+12]
 
-# TODO: plays a chord with root note. More options (in list?)?
-# settings = [major, minor, sept] 
-def play_chord(start, fs, rNote, pitch=0, settings=[0,0,0]):
+
+def play_chord(start, fs, rNote, settings = {'major':0, 'minor':0, '7':0}, pitch = 0):
     rNote = rNote + pitch
     chord = [rNote, rNote+7]
-    if settings[0]:
+    if settings['major']:
         chord.insert(1,rNote+4)
-    elif settings[1]: chord.insert(1,rNote+3)
-    elif settings[2]: chord.append(rNote+10)
+    if settings['minor']: chord.insert(1,rNote+3)
+    if settings['7']: chord.append(rNote+10)
     if start:
         for note in chord:
             fs.noteon(0, note, 127)
@@ -42,6 +44,10 @@ running = True
 bTone = generate_scale(50)    # Bass
 mTone = generate_scale(50+12) # Mid
 hTone = generate_scale(50+24) # High
+chord_settings = {'major':0, 'minor':0, '7':0}
+
+# True = Chords; False = Walking Bass
+chord_walking = True
 
 ## Key bindings
 
@@ -59,100 +65,160 @@ hTone = generate_scale(50+24) # High
 #  U ... Mediant
 #  O ... Submediant flat?
 
+
+
 while running:
     for event in pygame.event.get():
         if event.type == KEYDOWN:
-            #print event
-            if event.key == K_a:
-                play_chord(1, fs, bTone[0])
-                #fs.noteon(0, bTone[0], 127)
-                #fs.noteon(0, bTone[0]+7, 127)
+            
+            print event
+            # Play chords
+            if event.key == 49:
+                chord_walking = True
+            # Play Walking Bass Progression
+            if event.key == 50:
+                chord_walking = False
+            # Seventh modifier
+            if event.key == 304:
+                chord_settings['7'] = not chord_settings['7']
 
+            # Major chords
             if event.key == K_q:
-                fs.noteon(0, bTone[0], 127)
-                fs.noteon(0, bTone[0]+4, 127)
-                fs.noteon(0, bTone[0]+7, 127)
-
-            if event.key == K_s:
-                fs.noteon(0, bTone[0]+5, 127)
-                fs.noteon(0, bTone[0]+12, 127)
+                chord_settings['major']=1
+                play_chord(1,fs,bTone[0],chord_settings)
+                chord_settings['major']=0
                 
-            if event.key == K_d:
-                fs.noteon(0, bTone[0]+7, 127)
-                fs.noteon(0, bTone[0]+14, 127)
-                 
+            if event.key == K_w:
+                chord_settings['major']=1
+                play_chord(1,fs,bTone[2],chord_settings)
+                chord_settings['major']=0
+            if event.key == K_e:
+                chord_settings['major']=1
+                play_chord(1,fs,bTone[4],chord_settings)
+                chord_settings['major']=0
             if event.key == K_r:
-                fs.noteon(0, bTone[0]+8, 127)
-                fs.noteon(0, bTone[0]+15, 127)
-                
-            if event.key == K_f:
-                fs.noteon(0, mTone[4], 127)
-                
-            if event.key == K_g:
-                fs.noteon(0, mTone[0], 100)
-            if event.key == K_h:
-                fs.noteon(0, mTone[1], 127)
-            if event.key == K_j:
-                fs.noteon(0, mTone[2], 127)
-            if event.key == K_k:
-                fs.noteon(0, mTone[3], 127)
-            if event.key == K_l:
-                fs.noteon(0, mTone[4], 127)
-            if event.key == 232:
-                fs.noteon(0, mTone[5], 127)
-            if event.key == 230:
-                fs.noteon(0, mTone[6], 127)
-            if event.key == 190:
-                fs.noteon(0, mTone[7], 127)
-                
+                chord_settings['major']=1
+                play_chord(1,fs,bTone[4]+1,chord_settings)
+                chord_settings['major']=0
 
+
+            # Root + Fifth Chords
+            if event.key == K_a: play_chord(1, fs, bTone[0], chord_settings)
+            if event.key == K_s: play_chord(1, fs, bTone[2], chord_settings)
+            if event.key == K_d: play_chord(1, fs, bTone[4], chord_settings)
+            if event.key == K_f: play_chord(1, fs, bTone[4]+1,chord_settings)
+
+            # Minor Chords and Walking Bass
+            if event.key == 60 and not chord_walking: # key: <
+                fs.noteon(0, bTone[0],127)
+
+            if event.key == K_y:
+                if chord_walking:
+                    chord_settings['minor']=1
+                    play_chord(1,fs,bTone[0],chord_settings)
+                    chord_settings['minor']=0
+                else: fs.noteon(0, bTone[1]+1, 127)
+
+            if event.key == K_x:
+                if chord_walking:
+                    chord_settings['minor']=1
+                    play_chord(1,fs,bTone[2],chord_settings)
+                    chord_settings['minor']=0
+                else: fs.noteon(0, bTone[4], 127)                
+            if event.key == K_c:
+                if chord_walking:
+                    chord_settings['minor']=1
+                    play_chord(1,fs,bTone[4],chord_settings)
+                    chord_settings['minor']=0
+                else: fs.noteon(0, bTone[4]+2, 127)
+            if event.key == K_v:
+                if chord_walking:
+                    chord_settings['minor']=1
+                    play_chord(1,fs,bTone[4]+1,chord_settings)
+                    chord_settings['minor']=0
+                else: fs.noteon(0,bTone[5],127)
+            # Scale
+            if event.key == K_g: fs.noteon(0, mTone[0], 127)
+            if event.key == K_z: fs.noteon(0, mTone[1]-1, 127)
+            if event.key == K_h: fs.noteon(0, mTone[1], 127)
+            if event.key == K_u: fs.noteon(0, mTone[1]+1, 127)
+            if event.key == K_j: fs.noteon(0, mTone[2], 127)
+            if event.key == K_k: fs.noteon(0, mTone[3], 127)
+            if event.key == K_l: fs.noteon(0, mTone[4], 127)
+            if event.key == K_p: fs.noteon(0, mTone[4]+1, 127)
+            if event.key == 232: fs.noteon(0, mTone[5], 127)
+            if event.key == 230: fs.noteon(0, mTone[6], 127)
+            if event.key == 190: fs.noteon(0, mTone[7], 127)
+                
+            ## QUIT
             if event.key == K_ESCAPE:
                 pygame.quit()
-                sys.exit(1)
                 running = False
 
                 
         elif event.type == KEYUP:
-            
-            if event.key == K_a:
-                play_chord(0, fs, bTone[0])
-                #fs.noteoff(0, bTone[0])
-                #fs.noteoff(0, bTone[0]+7)
-
+            # Major Chords
             if event.key == K_q:
-                fs.noteoff(0, bTone[0])
-                fs.noteoff(0, bTone[0]+4)
-                fs.noteoff(0, bTone[0]+7)
-                
-            if event.key == K_s:
-                fs.noteoff(0, bTone[0]+5)
-                fs.noteoff(0, bTone[0]+12)
-                
-            if event.key == K_d:
-                fs.noteoff(0, bTone[0]+7)
-                fs.noteoff(0, bTone[0]+14)
+                chord_settings['major']=1
+                play_chord(0,fs,bTone[0],chord_settings)
+                chord_settings['major']=0
+            if event.key == K_w:
+                chord_settings['major']=1
+                play_chord(0,fs,bTone[2],chord_settings)
+                chord_settings['major']=0
+            if event.key == K_e:
+                chord_settings['major']=1
+                play_chord(0,fs,bTone[4],chord_settings)
+                chord_settings['major']=0
             if event.key == K_r:
-                fs.noteoff(0, bTone[0]+8)
-                fs.noteoff(0, bTone[0]+15)
-            
-            if event.key == K_f:
-                fs.noteoff(0, mTone[4])
-            if event.key == K_g:
-                fs.noteoff(0, mTone[0])
-            if event.key == K_h:
-                fs.noteoff(0, mTone[1])
-            if event.key == K_j:
-                fs.noteoff(0, mTone[2])
-            if event.key == K_k:
-                fs.noteoff(0, mTone[3])
-            if event.key == K_l:
-                fs.noteoff(0, mTone[4])
-            if event.key == 232:
-                fs.noteoff(0, mTone[5])
-            if event.key == 230:
-                fs.noteoff(0, mTone[6])
-            if event.key == 190:
-                fs.noteoff(0, mTone[7])
+                chord_settings['major']=1
+                play_chord(0,fs,bTone[4]+1,chord_settings)
+                chord_settings['major']=0
+            # Root + Fifth Chords
+            if event.key == K_a: play_chord(0, fs, bTone[0], chord_settings)    
+            if event.key == K_s: play_chord(0, fs, bTone[2], chord_settings)
+            if event.key == K_d: play_chord(0, fs, bTone[4], chord_settings)
+            if event.key == K_f: play_chord(0, fs, bTone[4]+1,chord_settings)
+            # Minor Chords and walking bass
+            if event.key == 60 and not chord_walking: # key: <
+                fs.noteoff(0, bTone[0])
+            if event.key == K_y:
+                if chord_walking:
+                    chord_settings['minor']=1
+                    play_chord(0,fs,bTone[0],chord_settings)
+                    chord_settings['minor']=0
+                else: fs.noteoff(0, bTone[1]+1)
+            if event.key == K_x:
+                if chord_walking:
+                    chord_settings['minor']=1
+                    play_chord(0,fs,bTone[2],chord_settings)
+                    chord_settings['minor']=0
+                else: fs.noteoff(0, bTone[4])
+            if event.key == K_c:
+                if chord_walking:
+                    chord_settings['minor']=1
+                    play_chord(0,fs,bTone[4],chord_settings)
+                    chord_settings['minor']=0
+                else: fs.noteoff(0, bTone[4]+2)
+            if event.key == K_v:
+                if chord_walking:
+                    chord_settings['minor']=1
+                    play_chord(0,fs,bTone[4]+1,chord_settings)
+                    chord_settings['minor']=0
+                else: fs.noteoff(0, bTone[5])
+
+            # Scale
+            if event.key == K_g: fs.noteoff(0, mTone[0])
+            if event.key == K_z: fs.noteoff(0, mTone[1]-1)
+            if event.key == K_h: fs.noteoff(0, mTone[1])
+            if event.key == K_u: fs.noteoff(0, mTone[1]+1)
+            if event.key == K_j: fs.noteoff(0, mTone[2])
+            if event.key == K_k: fs.noteoff(0, mTone[3])
+            if event.key == K_l: fs.noteoff(0, mTone[4])
+            if event.key == K_p: fs.noteoff(0, mTone[4]+1)
+            if event.key == 232: fs.noteoff(0, mTone[5])
+            if event.key == 230: fs.noteoff(0, mTone[6])
+            if event.key == 190: fs.noteoff(0, mTone[7])
            
 
 
